9e573f2873a1f73c6e46e184db3e72dd2f817d51
nokusukun <nokusukun@yahoo.co.jp>
2019-08-19 13:24:31 +0800

    feat(buildtools): Added client and server bootstrapping

--- public/electron.js
+++ public/electron.js
@@ -3,9 +3,44 @@ const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const isDev = require('electron-is-dev')
+var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
 
+var server
+var services
+// -- Services -- //
+// Execute the server and service first before loading anything else.
+if (process.platform.startsWith('win')) {
+  var extob = path.join("external", "openbazaard.exe");
+  var extserv = path.join("external", "services.exe");
+  if (isDev) {
+    extob = path.join("public", extob)
+    extserv = path.join("public", extserv) 
+    exec(`start ${extob} start --testnet`);
+    exec(`start ${extserv}`)
+  } else {
+    
+    server = spawn(extob,  ['start', '--testnet']);
+    services = spawn(extserv,  []);
+  }
+
+} else if (process.platform.startsWith('linux')) {
+  const extob = path.join("external", "openbazaard");
+  const extserv = path.join("external", "services");
+  if (isDev) {
+    extob = path.join("public", extob)
+    extserv = path.join("public", extserv) 
+    exec(`exec ${extob} start --testnet`);
+    exec(`exec ${extserv}`)
+
+  } else {
+    server = spawn(extob,  ['start', '--testnet']);
+    services = spawn(extserv,  []);
+  }
+}
+
+// -- Electron -- //
 let mainWindow
-
 const createWindow = () => {
   mainWindow = new BrowserWindow({
     title: 'Djali',
@@ -31,6 +66,10 @@ const createWindow = () => {
 }
 app.on('ready', createWindow)
 app.on('window-all-closed', () => {
+  if (!isDev) {
+    try { services.kill() } catch {}
+    try { server.kill() } catch {}
+  }
   if (process.platform !== 'darwin') {
     app.quit()
   }
